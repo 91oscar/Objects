76662a8b832a81050e4fffa1e34f174ceccac36f
Oscar Moran <oscar91moran@gmail.com>
2024-01-16 13:34:43 +0000

    first commit

---
+++ Object.js
@@ -0,0 +1,25 @@
+//Objects are a way to group information about a thing together.
+
+// Create an object called car with a make, model, and colour. Create a string detailing the car's make, model, and colour for a website advert and log it to the console
+const car = {
+  brand: `Kia`,
+  model: `Rio`,
+  colour: `Silver`,
+  engine: 1.4,
+  fuel: `gasoil`,
+};
+
+console.log(
+  `The brand of the car is ${car.brand} and the model is ${car.model}.`
+);
+
+// Create an object called book with a title, author, and number of pages. Create a string explaining the book's details for a website advert and log it to the console.
+const book = {
+  title: `I am Number 4`,
+  author: `Pittacus Lore`,
+  numberOfPages: 480,
+};
+
+console.log(
+  `My Favorite  book is ${book.title} because you won't be bored. the author is ${book.author} and the number of pages is ${book.numberOfPages}`
+);
